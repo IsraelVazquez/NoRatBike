1ca7928e58d24da36cf60b666cf2be99559f7cb5
IsraelVazquez <ivg1994.isc@gmail.com>
2020-12-13 16:29:35 -0600

    gps neo6 OK

---
+++ gps2.py
@@ -0,0 +1,23 @@
+import serial
+import time
+import string
+import pynmea2
+
+while True:
+    try:
+        port="/dev/ttyAMA0"
+        ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata=ser.readline()
+        if newdata[0:6] == "$GNGGA":
+            newmsg=pynmea2.parse(newdata)
+            lat=newmsg.latitude
+            lng=newmsg.longitude
+            gps = " Latitude= " + str(lat) + " and Longitude= " + str(lng)
+            print(gps)
+            ser.flush()
+            ser.close()
+            time.sleep(5)
+    except Exception:
+        print ("No connection")
+        
